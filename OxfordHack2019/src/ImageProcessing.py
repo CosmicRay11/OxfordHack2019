99e95c11ed37986370a67a64dbddf54213dedcc3
CosmicRay11 <george.w11@hotmail.co.uk>
2019-11-16 12:59:17 +0000

    Added functions for resizing and filtering by green colours

--- OxfordHack2019/src/ImageProcessing.py
+++ OxfordHack2019/src/ImageProcessing.py
@@ -5,16 +5,50 @@ Created on 16 Nov 2019
 '''
 
 import PIL
-import cv2
+import cv2 as cv
 import numpy as np
 
 class ImageProcessor(object):
-    '''
-    classdocs
-    '''
 
 
     def __init__(self, url):
         
-        self.image = 
+        self.initial = cv.imread(url)
+        
+        filteredIm = self.filter_green(self.initial)
+        
+        filteredIm =  self.resize(filteredIm, 0.2)
+        cv.imshow("filtered_green", filteredIm)
+        
+        
+        
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+        
+        
+    
+    def filter_green(self, im):
+        (b,g,r) = cv.split(im)
+        
+        m = np.maximum(np.maximum(b,g), r)
+        
+        b[True] = 0
+        g[g<m] = 0
+        r[True] = 0
+        
+        newIm = cv.merge([b,g,r])
+        
+        return newIm
+
+    def resize(self, im, factor):
+        return cv.resize(im, (0,0), fx=factor, fy=factor)
+    
+    
+
+if __name__ == "__main__":
+    defUrl = "C:\\Users\\George\\Documents\\University\\Hack prep\\2019-11\\IMG_20191112_190928.jpg"
+    i = ImageProcessor(defUrl)
+    
+    
+        
         
