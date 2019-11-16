d3807a396a3c6264a07328e54c4b60ae5226b8a4
CosmicRay11 <george.w11@hotmail.co.uk>
2019-11-16 12:32:30 +0000

    First commit

---
+++ OxfordHack2019/src/ImageProcessing.py
@@ -0,0 +1,20 @@
+'''
+Created on 16 Nov 2019
+
+@author: George
+'''
+
+import PIL
+import cv2
+import numpy as np
+
+class ImageProcessor(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, url):
+        
+        self.image = 
+        
