142c4a1ee07348554211ab3be09313ea9ce3ae83
CosmicRay11 <george.w11@hotmail.co.uk>
2019-11-16 14:45:51 +0000

    Added function to draw lines on top of the original image

--- OxfordHack2019/src/ImageProcessing.py
+++ OxfordHack2019/src/ImageProcessing.py
@@ -20,17 +20,22 @@ class ImageProcessor(object):
         greenIm = self.filter_green(self.initial)
         
         smallGreenIm = self.resize(greenIm, 0.2)
-        self.show_image("filtered_green", smallGreenIm)
+        #self.show_image("filtered_green", smallGreenIm)
         
         whiteFreeIm = self.remove_white(greenIm)
-        self.show_image('white filtered', whiteFreeIm)
+        #self.show_image('white filtered', whiteFreeIm)
         
         refilteredIm = self.exclude_rb(whiteFreeIm)
-        self.show_image('refiltered', refilteredIm)
+        #self.show_image('refiltered', refilteredIm)
         cv.imwrite(self.baseUrl + "\refiltered.png", refilteredIm)
         
         edgesIm = self.do_edge_detection(refilteredIm)
-        self.show_image('edges2', edgesIm)
+        #self.show_image('edges2', edgesIm)
+        
+        lineList = self.get_lines(edgesIm)
+        self.display_lines(self.initial, lineList)
+        
+        print(lineList)
         
         cv.waitKey(0)
         cv.destroyAllWindows()
@@ -67,6 +72,9 @@ class ImageProcessor(object):
         return newIm
 
     def exclude_rb(self, im):
+        # set green value for pixels that have too much red and blue to 0
+        # then cut out pixels with 0 green value
+        
         (b,g,r) = cv.split(im)
         
         m = np.minimum(np.minimum(b,g), r)
@@ -75,6 +83,7 @@ class ImageProcessor(object):
         
         newIm = cv.merge([b,g,r])
         
+        # the one here is important
         lowerFilter = np.array([0, 1,0])
         upperFilter = np.array([200,255,200])
         
@@ -84,15 +93,31 @@ class ImageProcessor(object):
         return newIm
     
     def do_edge_detection(self,im):
-        #edgeImage = cv.Canny(im, 5, 10)
-        #self.show_image("edge1", edgeImage)
-        #kernel = np.ones((10,10),np.float32)/10
-        #blurred = cv.filter2D(edgeImage,-1,kernel)
-        #self.show_image("blur", blurred)
-
-        self.show_image("newimage", im)
         newImage = cv.Canny(im, 100, 200)
         return newImage
+    
+    def get_lines(self, edgeImage):
+        #                     target,   rho, theta, something else?
+        lines = cv.HoughLines(edgeImage,1,np.pi/180,200)
+        return lines
+        
+    
+    def display_lines(self, im, lineList):
+        imCopy = im.copy()
+        for line in lineList:
+            for (rho,theta) in line:
+                lineLength = 10000
+                cT = np.cos(theta)
+                sT = np.sin(theta)
+                x0 = cT*rho
+                y0 = sT*rho
+                x1 = int(x0 + lineLength*(-sT))
+                y1 = int(y0 + lineLength*(cT))
+                x2 = int(x0 - lineLength*(-sT))
+                y2 = int(y0 - lineLength*(cT))
+            cv.line(imCopy,(x1,y1),(x2,y2),(0,0,255),2)
+        
+        self.show_image("line image", imCopy)
 
     def resize(self, im, factor):
         return cv.resize(im, (0,0), fx=factor, fy=factor)
@@ -104,8 +129,10 @@ if __name__ == "__main__":
     defUrl2 = "C:\\Users\\George\\Pictures\\Hack_tests\\IMG_20191116_100356.jpg"
     defUrl3 = "C:\\Users\\George\\Pictures\\Hack_tests\\IMG_20191115_191217.jpg"
     defUrl4 = "C:\\Users\\George\\Pictures\\Hack_tests\\IMG_20191115_191345.jpg"
-    i = ImageProcessor(defUrl4)
+    urls = [defUrl, defUrl2, defUrl3, defUrl4]
+    for url in urls:
+        i = ImageProcessor(url)
     
-    
+
         
         
