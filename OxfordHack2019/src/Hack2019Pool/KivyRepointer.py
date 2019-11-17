808ded7322ce665e5fd572ddb2644044eb0dd70b
CosmicRay11 <george.w11@hotmail.co.uk>
2019-11-17 11:05:19 +0000

    Partway through merging

--- OxfordHack2019/src/Hack2019Pool/KivyRepointer.py
+++ OxfordHack2019/src/Hack2019Pool/KivyRepointer.py
@@ -660,7 +660,10 @@ class Projector(object):
 #----------------------------------------------------------------------------------------------------
 
 def transform_coordinates(pos):
-        return (pos[0]*3+47,pos[1]*2+47)
+        return (2.2*pos[0]+12,3.5*pos[1] -180)
+    
+    
+    #
 
 class PoolTable(Widget):
     
@@ -683,15 +686,13 @@ class PoolTable(Widget):
         
     def drawBall(self, ball):
         with self.canvas:
+            ball.color
             Ellipse(pos=(self.lower_pos[0]+ball.pos[0]+ball.radius,self.lower_pos[1]+ball.pos[1]+ball.radius), size=(ball.radius, ball.radius))
       
     def drawBalls(self):
         for ball in self.balls:
-            with self.canvas:
-                ball.color
-                Ellipse(size=(ball.radius, ball.radius), pos=(self.lower_pos[0]+ball.pos[0]+ball.radius,self.lower_pos[1]+ball.pos[1]+ball.radius))
-
-            
+            ball.display(self)
+                       
     def addBalls(self,manyballs):
         self.canvas.clear()
         with self.canvas:
@@ -704,15 +705,16 @@ class PoolTable(Widget):
 class Ball():
     def __init__(self, color,position):
         super(Ball, self).__init__()
-        self.radius = 4*5.7
+        self.radius = 2*5.7
         self.color = color
         self.pos = transform_coordinates(position)
         print(self.pos)
         
     def display(self,a_widget):
+        print(self.color)
         with a_widget.canvas:
-            self.color
-            Ellipse(color= self.color, pos=(a_widget.lower_pos[0]+self.pos[0]+self.radius,a_widget.lower_pos[1]+self.pos[1]+self.radius), size=(self.radius, self.radius))
+            Color(self.color[0], self.color[1], self.color[2])
+            Ellipse(pos=(a_widget.lower_pos[0]+self.pos[0]+self.radius,a_widget.lower_pos[1]+self.pos[1]+self.radius), size=(self.radius, self.radius))
             
           
 #===============================================================================
@@ -770,7 +772,7 @@ class MainScreen(GridLayout):
 
         #self.camera = Camera(play = True, resolution = (960,640), size_hint_x = 1.5)
         
-        url = "C:\\Users\\George\\Pictures\\Hack_tests\\IMG_20191115_191414.jpg"
+        url = "C:\\Users\\George\\Pictures\\Hack_tests\\IMG_20191115_191414.jpg" #  "C:\\Users\\George\\Pictures\\Hack_tests\\IMG_20191116_100356.jpg"
         self.camera = Image(source = url)
         self.top_row.add_widget(self.camera)
         self.top_widgets.append(self.camera)
@@ -797,6 +799,8 @@ class MainScreen(GridLayout):
             if self.button_camera.text == "Take a pic!":
                 timestr = time.strftime("%Y%m%d_%H%M%S")
                 url = "IMG_{}.png".format(timestr)
+                
+                #url = "C:\\Users\\George\\Pictures\\Hack_tests\\IMG_20191116_100356.jpg"
                 url = "C:\\Users\\George\\Pictures\\Hack_tests\\IMG_20191115_191414.jpg"
                 print('changed')
                 self.button_camera.text = "Loading..."
@@ -822,13 +826,14 @@ class MainScreen(GridLayout):
                         y = int(self.ballList[i][1] * 100)
                         col = self.ballList[i][2]
                         if col == 'R':
-                            col = Color(1,1,0)
+                            col = [1,1,0]
                         elif col == "Y":
-                            col = Color(1,0,0)
+                            col = [1,0,0]
                         elif col == "B":
-                            col = Color(0,0,0)
+                            col = [0,0,0]
                         else:
-                            col = Color(1,1,1)
+                            col = [1,1,1]
+                        
                         self.ballList[i][2] = col
                     
                         self.ballList[i] = Ball(col, (x,y))
@@ -873,17 +878,43 @@ class MainScreen(GridLayout):
 
         def null_function(instance):
             return None
-        
-        
 
         def calculate(instance):
             self.button_calculate.bind(on_press = null_function)
             self.button_calculate.text = "Loading..."
+            # give self.ballList to foo()
+            
+            
+            for i, ball in enumerate(self.ballList):
+                col = ball.color
+                if col == [1,1,0]:
+                    col = 'red'
+                elif col == [1,0,0]:
+                    col = 'yellow'
+                elif col == [0,0,0]:
+                    col = 'black'
+                else:
+                    col = 'white'
+                
+                self.ballList[i] = (ball.pos[0], ball.pos[1], col)
+                
+            
+            cue = turn(self.player.lower(), self.ballList)
+    
+            
+            
+            #cuePoint = transform_coordinates(cuePoint)
+            #whitePoint = transform_coordinates(whitePoint)
+            #pocketPoint = transform_coordinates(pocketPoint)
+            
+            with self.canvas:
+                Line(points=[100, 100, 300, 400])
+                #Line(points=[cuePoint[0], cuePoint[1], 
+            
             self.button_calculate.bind(on_press = calculate)
             self.button_calculate.text = "Calculate!"
         
         self.button_calculate.bind(on_press = calculate)
-
         self.button_player = Button(text="Player: " + self.player)
         self.bottom_row.add_widget(self.button_player)
         self.bottom_widgets.append(self.button_player)
@@ -895,9 +926,383 @@ class MainScreen(GridLayout):
 
  
  
+#-------------------------------------------------------------------------------
+
+
+#Input = position of every ball and dimension of table
+#Output = optimal shot coordinates
 
 
 
+
+
+import math
+
+
+
+
+info = {
+    "resolution" : 1,
+    "dimensions" : [100,200],
+    "corner_len" : 8,
+    "side_len" : 6,
+}
+
+
+
+#useful functions for geometry
+
+def euclidean_dist(pos1,pos2):
+    return ((pos1[1]-pos2[1])**2 + (pos1[0]-pos2[0])**2)
+
+def slope(pos1,pos2):
+    if pos1[0]==pos2[0]:
+        return 1000
+    else:
+        
+        return (pos1[1]-pos2[1])/(pos1[0]-pos2[0])
+
+
+g = 9.81
+mu = 0.6
+
+def affine(m,p,x):
+    return (m*x+p)
+
+def in_circle(center,point, radius):
+    if euclidean_dist(center,point)<radius**2:
+        return True
+    else:
+        return False
+    
+def is_between(m,top_p,bot_p,point):
+    x = point[0]
+    y = point[1]
+    if (affine(m,bot_p,x) < y < affine(m, top_p, x)):
+        return True
+    else:
+        return False
+    
+def angle(point1,point2):
+    angle = math.atan(slope(point1,point2))
+    if point2[0]>point1[0]:
+        return angle
+    else:
+        return (pi-angle)
+
+
+
+
+class Ball2:
+    def __init__(self, pos, colour):
+        
+        #position is already scaled
+        self.pos = pos
+        self.colour = colour
+        self.r = 2.8
+        self.res = info["resolution"]
+        
+        
+    def pixels(self):
+        # assign the pixels to the ball , radius = 5.7
+        r = round(self.res*self.r)
+        x = self.pos[0]
+        y = self.pos[1]
+        pixels = [[x+r,y], [x-r, y], [x, y+r], [x, y-r], [x+round(r/math.sqrt(2)), y + round(r/math.sqrt(2))], [x-round(r/math.sqrt(2)), y + round(r/math.sqrt(2))],[x+round(r/math.sqrt(2)), y - round(r/math.sqrt(2))],[x - round(r/math.sqrt(2)), y - round(r/math.sqrt(2))]]
+        return pixels
+        
+    def hole_dist(self, hole_pos):
+        return math.sqrt(euclidean_dist(self.pos,hole_pos))
+    
+    def cue_dist(self, cue_ball):
+        return math.sqrt(euclidean_dist(self.pos, cue_ball.pos))
+    
+    def get_angle(self, cue_ball, hole_pos):
+        m1 = slope(self.pos, cue_ball.pos)
+        m2 = slope(hole_pos, self.pos)
+        return math.atan(abs((m2-m1)/(1+m1*m2)))
+    
+    def initial_force(self, cue_ball, hole_pos):
+        angle = self.get_angle(cue_ball, hole_pos)
+        initial_force = 2*mu*g*(self.cue_dist(cue_ball)+(2*mu*g*self.hole_dist(hole_pos))/(math.sin(angle/2))**2)
+        return initial_force
+    
+    def actual_target(self,cue,hole_pos):
+        r = self.r*self.res
+        angle2 = angle(self.pos,hole_pos)
+        target = [self.pos[0]-2*r*math.cos(angle2), self.pos[1]-2*r*math.sin(angle2)] # does this work with signs?
+        target = [round(target[0]), round(target[1])]
+        
+        return target
+    
+    def is_possible(self, cue_ball, hole_pos, balls, hole_bounds): #balls is a list of all balls except the two concerned
+        #find actual equation of shot (with adjustment)
+        # 1: find adjusted target
+        r = self.r*self.res
+        angle2 = angle(self.pos,hole_pos)
+        target = self.actual_target(cue_ball,hole_pos)
+        
+        #find equation
+        m = slope(cue_ball.pos, target)
+        p = m*target[0] + target[1]
+        
+        #find boundaries
+        angle1 = angle(cue_ball.pos, target)+0.01
+        shift1 = abs(2*r/math.sin(angle1))
+        top_p = p + m*shift1
+        bot_p = p - m*shift1
+        
+        #iterations for checking
+        #check cue to ball
+        for ball in balls:
+            for point in ball.pixels():
+                if is_between(m, top_p, bot_p, point) and is_between(0,max(cue_ball.pos[0],ball.pos[0]), min(cue_ball.pos[0],ball.pos[0]), point):
+                    return False
+                
+        #check ball to hole
+        #find new line
+        
+        M = slope(self.pos, hole_pos)
+        P = M*hole_pos[0] + hole_pos[1] 
+        
+        shift2 = abs(2*r/math.sin(angle2))
+        top_P = P + M*shift2
+        bot_P = P - M*shift2
+        
+        #check ball to hole
+        for ball in balls:
+            for point in ball.pixels():
+                if is_between(M, top_P, bot_P, point) and is_between(0,max(ball.pos[0],hole_pos[0]), min(ball.pos[0],hole_pos[0]), point):
+                    return False
+                
+        # narrower boundaries when we check the pocket boundaries
+        shift3 = shift2/2
+        top_P = P + M*shift3
+        bot_P = P - M*shift3
+        for hole in hole_bounds:
+            for point in hole_bounds[hole]:
+                if is_between(M, top_P, bot_P, point) and is_between(0,max(ball.pos[0],hole_pos[0]), min(ball.pos[0],hole_pos[0]), point):
+                    return False
+            
+        return True
+    
+
+def h_bounds(info):
+    s = info["resolution"]*info["corner_len"]
+    r = info["resolution"]*info["side_len"]
+    [wdt,lth] = info["dimensions"]
+    hole_bounds = {
+        "1" : [[s,0],[0,s]],
+        "2" : [[1,lth//2-r],[1,lth//2+r]],
+        "3" : [[s,lth-1],[1,lth-s]],
+        "4" : [[wdt-s,lth-1],[wdt-1,lth-s]],
+        "5" : [[lth//2-r,wdt-1],[lth//2+r,wdt-1]],
+        "6" : [[wdt-s,1],[wdt-1,s]]
+    }
+    
+    return hole_bounds
+
+
+
+#create dic of target points for pocket
+def t_points(info):
+    s = info["resolution"]*info["corner_len"]
+    r = info["resolution"]*info["side_len"]
+    res = info["resolution"]
+    
+    
+    
+    [wdt,lth] = info["dimensions"]
+    target_points = [[0 for i in range(3)] for j in range(6)]
+    target_points = {
+        "1" : [[s//4,(3*s)//4],[s//2,s//2], [(3*s)//4,s//4]],
+        "2" : [[1,lth//2-r//2],[1,lth//2],[1,lth//2+r//2]],
+        "3" : [[s//4,lth-(3*s)//4],[s//2,lth-s//2],[(3*s)//4,lth-s//4]],
+        "4" : [[wdt-(3*s)//4,lth-s//4],[wdt-s//2,lth-s//2],[wdt-s//4,lth-(3*s)//4]],
+        "5" : [[wdt-1,lth//2-r//2],[wdt-1,lth//2],[wdt-1,lth//2+r//2]],
+        "6" : [[wdt-(3*s)//4,s//4],[wdt-s//2,s//2], [wdt-s//4,(3*s)//4]],
+    }
+    
+    return target_points
+
+
+hole_bounds = h_bounds(info)
+target_points = t_points(info)
+
+
+
+import scipy.integrate as integrate
+import scipy.special as special
+from scipy.stats import *
+from pynverse import inversefunc
+from math import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+def prob(ball_r,balls_dist,angle_1,angle_2,cue_ball_angle,omega):
+
+    min_in_angle = min(angle_1,angle_2)
+    max_in_angle = max(angle_1,angle_2)
+
+    def f(t):
+        try:
+            return asin(balls_dist*sin(t)/(2*ball_r)) - abs(t) + cue_ball_angle 
+        except:
+            pass
+    
+    def pdf(x):
+        var = 0.1 #* exp(-(balls_dist-ball_r*2))
+        return pow(2*pi*var,-0.5) * exp(-((x-omega)**2)/(2*var))
+    
+    try:
+        bound = asin(2*ball_r/balls_dist)
+    except:
+        return 0
+
+    f_image = [min_in_angle,max_in_angle]
+    inv_f = inversefunc(f,domain=[-1*bound,bound], image = f_image)
+
+
+    return integrate.quad(pdf,inv_f(f_image[0]),inv_f(f_image[1]))[0]
+
+
+#finds the integration bounds
+def target_angle_bounds(ball, hole_nb):
+    point1, point2 = hole_bounds[hole_nb][0], hole_bounds[hole_nb][1]
+    angle1, angle2 = angle(ball.pos,point1), angle(ball.pos, point2) 
+    return (angle1,angle2)
+   
+
+
+
+#For a given cue and colour, iterate the list
+
+def find_optimal_direct(balls,cue,colours):
+    shots = []
+    radius = balls[0].r*balls[0].res
+    
+    for ball in balls:
+        if ball.colour in colours:
+            for hole in target_points:
+                (angle1, angle2) = target_angle_bounds(ball,hole)
+                cue_angle = angle(cue.pos,ball.pos)
+                
+                for hole_point in target_points[hole]:
+                    
+                    target = ball.actual_target(cue, hole_point)
+                    
+                    omega = cue_angle - angle(target,hole_point)
+                    if ball.hole_dist(hole_point)<=cue.hole_dist(hole_point):
+                        
+                        proba = prob(radius, ball.cue_dist(cue), angle1, angle2, cue_angle, omega)
+                    else:
+                        proba = 0
+                    
+                    shots.append([ball,hole,hole_point,proba])
+                 
+    shots.sort(key=lambda x : x[3], reverse=True)
+
+    
+    for shot in shots:
+        ball = shot[0]
+        hole_point = shot[2]
+        if ball.is_possible(cue,hole_point,balls,hole_bounds):
+            if shot[3] > 0:
+                optimal_shot = {
+                    "cue position" : cue.pos,
+                    "ball position" : ball.pos,
+                    "collision point" : ball.actual_target(cue, hole_point),
+                    "target hole" : hole_point,
+                    "probability" : shot[3],
+                    "bounce" : False
+                } 
+
+                return optimal_shot
+            
+            
+        
+    return "no available shot"
+
+
+
+def check_bounce_shot(balls,cue,colours):
+        
+    [wdt,lth] = info["dimensions"]        
+    
+    def Y(x,a):
+        return (a*y+b*x)/(a+x)
+    
+    def X(y,b):
+        return (x*y+a*b)/(b+y)
+    
+    for ball in balls:
+        if ball.colour in colours:
+            [x,y] = cue.pos
+            [a,b] = ball.pos
+            cue1 = Ball2([1,Y(x, a)],"white")
+            cue2 = Ball2([1,Y(wdt - x, wdt - a)],"white")
+            cue3 = Ball2([X(y,b),1],"white")
+            cue4 = Ball2([X(lth-y, lth-b),1],"white")
+            
+            optimals = [find_optimal_direct(balls,cue1,colours), find_optimal(balls,cue2,colours), find_optimal(balls,cue3,colours), find_optimal(balls,cue4,colours),]
+            
+            optimals.sort(key = lambda x: x["probability"], reverse=True)
+            optimals[0]["bounce"] = True
+            return optimals[0]
+
+
+
+def find_optimal(balls,cue,colours):
+    direct_best = find_optimal_direct(balls,cue,colours)
+    if direct_best == "no available shot":
+        return check_bounce_shot(balls,cue,colours)
+    else:
+        return direct_best
+
+
+
+#if white ball can be placed
+def place_white(balls,colour):
+    radius = round(balls[0].r*balls[0].res)
+    [wdt,lth] = info["dimensions"]
+    optimals = []
+    for delta_x in range(radius,wdt-radius,4*radius):
+        for delta_y in range(radius,lth//4-radius,4*radius):
+            cue = Ball2([delta_x,delta_y],"white")
+            optimals.append(find_optimal(balls,cue,colour))
+    optimals.sort(key = lambda x: x["probability"], reverse=True)
+    return optimals[0]
+
+
+
+
+
+#===============================================================================
+# 
+# #For each turn
+# colour = input() # can be ["red","yellow"] if table is open
+# white_on_table = bool(input())
+#===============================================================================
+def turn(colours, ball_coords):
+    balls = []
+    cue = None
+    for ball in ball_coords:
+        balls.append(Ball2(ball[0],ball[1]))
+        if ball[1] == 'white':
+            cue = Ball2(ball[0],ball[1])
+    #balls is a list of elements of the class ball
+    
+    if cue != None:
+        return find_optimal(balls,cue,colours)
+    else:
+        return place_white(balls,colours)
+
+
+
+
+
+
+#-------------------------------------------------------------------------------
 
 class MyApp(App):
 
